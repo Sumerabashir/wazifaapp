0f71a4d2e3901d654c2da5565b0052142e1dd77c
sumerabashir <sumera.bashir1982@gmail.com>
2019-06-01 05:27:16 +0500

    wazifaapp

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,9 @@
+
+
+
+function onDeviceReady(){
+	admob.createBannerView({publisherId: "ca-app-pub-6720216883640190/1313174725"});
+
+}	document.addEventListener('deviceready', onDeviceReady, false);
+
+
